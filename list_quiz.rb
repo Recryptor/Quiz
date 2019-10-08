9b3521b46ac21d2d4230bbffb69b3dfc726bd384
Dashel Bradshaw <Bradshawdc@s.dcsdk12.org>
2019-10-08 08:56:03 -0600

    Finished three_even and bigger_two.

--- list_quiz.rb
+++ list_quiz.rb
@@ -21,8 +21,31 @@ def three_even?(list)
 
 end
 
+def bigger_two(list, list2)
+    sum1 = list[0] + list[1]
+    sum2 = list2[0] + list2[1]
+
+    if sum1 > sum2 || sum1 = sum2
+        print list
+    elsif sum1 < sum2
+        print list2
+    end
+end
+
+
+
+puts bigger_two([1, 2], [3, 4])
+puts bigger_two([1, 7], [4, 4])
+puts bigger_two([1, 7], [2, 4])
+
+
 puts three_even?([2, 1, 3, 5])
 puts three_even?([2, 4, 12, 5])
 puts three_even?([2, 1, 4, 6])
 puts three_even?([1, 4, 6, 4])
 puts three_even?([])
+
+puts "\n"
+puts "\n"
+puts "\n"
+puts "\n"
