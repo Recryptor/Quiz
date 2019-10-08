2cdae343be99c2c4d9673e14c7a8c5dc8539cdb7
Dashel Bradshaw <Bradshawdc@s.dcsdk12.org>
2019-10-08 09:19:42 -0600

    Finished all challenges. Three_even, bigger_two, and series_up.

--- list_quiz.rb
+++ list_quiz.rb
@@ -32,20 +32,34 @@ def bigger_two(list, list2)
     end
 end
 
+def seriesup(count)
+    series = []
+    first = 1
+    count.times do |i| 
 
-
-puts bigger_two([1, 2], [3, 4])
-puts bigger_two([1, 7], [4, 4])
-puts bigger_two([1, 7], [2, 4])
-
-
+        series.push(first)
+        i.times do |j|      
+                series.push(j + 2)
+        end
+    end
+    print series
+end
+#J is the variable wanted to push. but first, you must add a few to compensate for the fact that it can't start at 0. If I don't do that, it goes [1,1,0,1,0,1,1,0,1,2], which isn't correct.
+#First j = 0   J + 1 is one, printing 1,1.  J + 2 = 2, which is the first number I want to add past 1. 
 puts three_even?([2, 1, 3, 5])
 puts three_even?([2, 4, 12, 5])
 puts three_even?([2, 1, 4, 6])
 puts three_even?([1, 4, 6, 4])
 puts three_even?([])
-
 puts "\n"
 puts "\n"
+puts bigger_two([1, 2], [3, 4])
+puts bigger_two([1, 7], [4, 4])
+puts bigger_two([1, 7], [2, 4])
 puts "\n"
 puts "\n"
+puts seriesup(1)
+puts seriesup(2)
+puts seriesup(3)
+puts seriesup(4)
+puts seriesup(5)
